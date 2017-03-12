2aef4f41e6ec662e6db1f6fb81fa5da5714edd9c
rupengkun <522523828@qq.com>
2017-03-12 21:16:36 +0800

    1.03 Create an AsyncTask and fetch the json data from the movie db

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -1,19 +1,30 @@
 package com.example.android.popularmovies;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.net.URL;
+
+import utilities.NetworkUtils;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mDisplayQueryResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDisplayQueryResult = (TextView)findViewById(R.id.tv_display_query_result);
     }
 
     // Inflater our menu resource
@@ -28,9 +39,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int menuItemThatWasSelected = item.getItemId();
         if(menuItemThatWasSelected==R.id.action_refresh){
-            Context context = MainActivity.this;
-            String message = "Refresh clicked";
-            Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
+            makeTheMovieDbSearchQuery();
         }
         if(menuItemThatWasSelected==R.id.action_setting){
             Context context = MainActivity.this;
@@ -38,5 +47,36 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void makeTheMovieDbSearchQuery(){
+        URL theMovieDbSearchUrl = NetworkUtils.buildUrl("popularity");
+        new TheMovieDbQueryTask().execute(theMovieDbSearchUrl);
+    }
+
+    //Fetch the popular movie data from internet on the background thread
+    public class TheMovieDbQueryTask extends AsyncTask<URL,Void,String>{
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchUrl = urls[0];
+            String themoviedbSearchResults = null;
+
+            try {
+                themoviedbSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return themoviedbSearchResults;
+        }
+
+
+        @Override
+        protected void onPostExecute(String s) {
+            if(s!=null&&!s.equals("")){
+                mDisplayQueryResult.setText(s);
+            }
+        }
     }
 }
---
+++ app/src/main/java/utilities/NetworkUtils.java
@@ -0,0 +1,66 @@
+package utilities;
+
+/**
+ * Created by kun on 2017/3/12.
+ */
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * these utilities will be used to communicate with THE MOVIE DB servers
+ */
+public class NetworkUtils {
+
+    private static final String TAG_LOG = NetworkUtils.class.getName();
+
+    private static final String THE_MOVIE_DB_BASIC_URI = "https://api.themoviedb.org/3/movie/popular?api_key=7cd08965da83865e7aca5470202011e9";
+
+    final static String PARAM_SORT = "sort";
+
+    public static URL buildUrl(String sortBy){
+        Uri builtUri = Uri.parse(THE_MOVIE_DB_BASIC_URI).buildUpon()
+                .appendQueryParameter(PARAM_SORT,sortBy)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    /**
+     * This method returns the entire results from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
