d6ec3ed857225373bc1ddd2b3e27f163c89c9858
rupengkun <522523828@qq.com>
2017-03-12 18:32:58 +0800

    1.02 Add a Menu(popular_movie.xml)

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Inflater our menu resource
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.popular_movie,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemThatWasSelected = item.getItemId();
+        if(menuItemThatWasSelected==R.id.action_refresh){
+            Context context = MainActivity.this;
+            String message = "Refresh clicked";
+            Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
+        }
+        if(menuItemThatWasSelected==R.id.action_setting){
+            Context context = MainActivity.this;
+            String message = "Setting clicked";
+            Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
