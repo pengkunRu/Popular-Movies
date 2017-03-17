7b671d614193323c1543cb59c16add0d4287dafb
rupengkun <522523828@qq.com>
2017-03-18 03:14:01 +0800

    1.23 Passing Data from MainActivity to DetailActivity

--- app/src/main/java/com/example/android/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/android/popularmovies/DetailActivity.java
@@ -1,13 +1,19 @@
 package com.example.android.popularmovies;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
 
+    private static final String TAG_LOG = DetailActivity.class.getName();
+
+    private TextView mDisplayDetailInfomation;
+    private MovieInformation mCurrentMovie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +27,29 @@ public class DetailActivity extends AppCompatActivity {
                 Toast.makeText(DetailActivity.this,"Add your Action!",Toast.LENGTH_SHORT).show();
             }
         });
+
+        mDisplayDetailInfomation = (TextView)findViewById(R.id.tv_display_detail_information);
+
+        /**
+         * Get the intent that created this Activity, then we can retrieve data
+         * That passed in it. pretty much like opening the envelope
+         */
+        Intent intentThatStartThisActivity = getIntent();
+
+        /**
+         * Activities always has an Intent which triggers to stop that Activity
+         * but, It didn't have any data in it. so you shouldn't assume that every
+         * intent has extra data and always check if it does.
+         */
+        if(intentThatStartThisActivity.hasExtra(Intent.EXTRA_TEXT)){
+            mCurrentMovie = intentThatStartThisActivity.getParcelableExtra(Intent.EXTRA_TEXT);
+            mDisplayDetailInfomation.setText(mCurrentMovie.getmTitle()+"\n\n"+
+                            mCurrentMovie.getmReleaseDate()+"\n\n"+
+                            mCurrentMovie.getmPopularity()+"\n\n"+
+                            mCurrentMovie.getmPosterPath()+"\n\n"+
+                            mCurrentMovie.getmVoteAverage()+"\n\n"+
+                            mCurrentMovie.getmOverview());
+        }
     }
+
 }
